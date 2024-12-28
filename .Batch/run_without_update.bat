1a1239c926a4958bb9455e43bdaf521df60b0416
David Sage <djwDragonsage@telus.net>
2024-12-27 23:41:09 -0800

    Initiated Batch Files
    Startup batch files are located in the .Batch subfolder of the FooocusPlus main folder.

---
+++ .Batch/run_without_update.bat
@@ -0,0 +1,4 @@
+@echo off
+..\python_embeded\python.exe -s ..\entry_without_update.py --models-root ../models --output-path ../outputs  --language en
+echo All done.
+pause
---
+++ .Batch/常规启动.bat
@@ -0,0 +1,5 @@
+@echo off
+..\python_embeded\python.exe -s ..\entry_with_update.py --models-root ../models --output-path ../outputs --language cn
+
+echo All done.
+pause
