1dc6c61f53471630bee301eece6b9c87d55daa9d
David Sage <djwDragonsage@telus.net>
2024-12-29 09:11:33 -0800

    Removed Python from Repo

--- .Batch/run_FooocusPlus.bat
+++ .Batch/run_FooocusPlus.bat
@@ -1,4 +1,4 @@
 @echo off
-..\python_embeded\python.exe -s ..\entry_with_update.py --models-root ../models --output-path ../outputs --language en
+..\..\python_embeded\python.exe -s ..\entry_with_update.py --models-root ../models --output-path ../outputs --language en
 echo All done.
 pause
