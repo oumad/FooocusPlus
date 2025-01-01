c19f5450b656119c07875e5055c794f89eb32062
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-01 12:38:44 -0800

    Update topbar.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -137,13 +137,10 @@ def get_system_message():
 
 
 def preset_instruction():
-    lang=system_params["__lang"];
-    if (lang=="cn") {
-        head = "<div style='max-width:100%; max-height:86px; overflow:hidden'>"
-        foot = "</div>"
-        body = '预置包简介:<span style="position: absolute;right: 0;"><a href="https://gitee.com/metercai/SimpleSDXL/blob/SimpleSDXL/presets/readme.md">\U0001F4DD 什么是预置包</a></span>'
-        body += f'<iframe id="instruction" src="{get_preset_inc_url()}" frameborder="0" scrolling="no" width="100%"></iframe>'
-    }
+    head = "<div style='max-width:100%; max-height:86px; overflow:hidden'>"
+    foot = "</div>"
+    body = '预置包简介:<span style="position: absolute;right: 0;"><a href="https://gitee.com/metercai/SimpleSDXL/blob/SimpleSDXL/presets/readme.md">\U0001F4DD 什么是预置包</a></span>'
+    body += f'<iframe id="instruction" src="{get_preset_inc_url()}" frameborder="0" scrolling="no" width="100%"></iframe>'
     return head + body + foot
 
 
