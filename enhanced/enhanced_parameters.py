b5209fd96d4b3bbe8db38822a7f0106016c75771
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-02 14:33:58 -0800

    Update enhanced_parameters.py set_preselector is separate function
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/enhanced_parameters.py
+++ enhanced/enhanced_parameters.py
@@ -1,4 +1,4 @@
-backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = [None] * 4
+backfill_prompt, translation_methods, comfyd_active_checkbox = [None] * 3
 
 def set_all_enhanced_parameters(*args):
     global backfill_prompt, translation_methods, comfyd_active_checkbox, preselector
@@ -10,4 +10,13 @@ def set_all_enhanced_parameters(*args):
 
     backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = args
 
+    return
+
+def set_preselector():
+    global preselector
+    if preselector == '':
+        if lang=='cn':
+            preselector='Topbar Menu'
+        else:    
+            preselector='Dropdown Menu'
     return
