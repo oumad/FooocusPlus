401b422c8012112e91664b4105f60c3c22c2cf98
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-01 17:42:37 -0800

    Update args_manager.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- args_manager.py
+++ args_manager.py
@@ -6,8 +6,8 @@ args_parser.parser.add_argument("--disable-preset-selection", action='store_true
                                 help="Disables preset selection in Gradio.")
 
 # args_parser.parser.add_argument("--language", type=str, default='cn',
-                                help="Translate UI using json files in [language] folder. "
-                                  "For example, [--language example] will use [language/example.json] for translation.")
+#                                help="Translate UI using json files in [language] folder. "
+#                                  "For example, [--language example] will use [language/example.json] for translation.")
 
 args_parser.parser.add_argument("--webroot", type=str, default='', help="Set the webroot path.")
 args_parser.parser.add_argument("--location", type=str, default='CN', help="Set the location access location.")
