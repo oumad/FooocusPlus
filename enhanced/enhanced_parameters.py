b6bb1b0845bd5fdbe463ac4ead87ef4f8a8c2dcb
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-02 13:41:17 -0800

    Update enhanced_parameters.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/enhanced_parameters.py
+++ enhanced/enhanced_parameters.py
@@ -2,11 +2,11 @@ backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = [Non
 
 def set_all_enhanced_parameters(*args):
     global backfill_prompt, translation_methods, comfyd_active_checkbox, preselector
-        if preselector = '':
-            if lang=='cn':
-                'preselector': 'Topbar Menu'
-            else:    
-                'preselector': 'Dropdown Menu'
+    if preselector = '':
+        if lang=='cn':
+            'preselector': 'Topbar Menu'
+        else:    
+            'preselector': 'Dropdown Menu'
 
     backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = args
 
