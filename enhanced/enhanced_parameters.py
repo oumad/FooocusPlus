e14420478d707669bcf834dfc453cc474cf81ff1
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-02 12:21:59 -0800

    Update enhanced_parameters.py Added preselector
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/enhanced_parameters.py
+++ enhanced/enhanced_parameters.py
@@ -1,8 +1,8 @@
-backfill_prompt, translation_methods, comfyd_active_checkbox = [None] * 3
+backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = [None] * 4
 
 def set_all_enhanced_parameters(*args):
-    global backfill_prompt, translation_methods, comfyd_active_checkbox
+    global backfill_prompt, translation_methods, comfyd_active_checkbox, preselector
 
-    backfill_prompt, translation_methods, comfyd_active_checkbox = args
+    backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = args
 
     return
