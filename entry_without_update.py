ee60f9505eea480c9539ff3f3c3242fabf160f77
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-03 13:18:24 -0800

    Update entry_without_update.py: added console msg
    print('Loading FooocusPlus without updating...')
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- entry_without_update.py
+++ entry_without_update.py
@@ -6,6 +6,8 @@ root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root)
 os.chdir(root)
 
+print('Loading FooocusPlus without updating...')
+
 #if __name__ == '__main__':
 #    import multiprocessing as mp
 #    mp.set_start_method('spawn')
