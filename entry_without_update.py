98cb52acd3d1447e1e3b464f59b9b99fb0133aaa
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-03 11:29:53 -0800

    Update entry_without_update.py REM multiprocessing
    And test removal of "from launch import *" for main loop testing
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- entry_without_update.py
+++ entry_without_update.py
@@ -6,8 +6,8 @@ root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(root)
 os.chdir(root)
 
-if __name__ == '__main__':
-    import multiprocessing as mp
-    mp.set_start_method('spawn')
+#if __name__ == '__main__':
+#    import multiprocessing as mp
+#    mp.set_start_method('spawn')
 
-from launch import *
+#from launch import *
