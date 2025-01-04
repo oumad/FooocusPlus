4ca08bb398e0cc4fe1f33d5c9d77e3c9e61807f1
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-04 15:21:35 -0800

    Update launch.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -76,8 +76,8 @@ def check_base_environment():
 
     print("Checking resources...")
     from enhanced.simpleai_base import simpleai_base
-    #token = simpleai_base.init_local(f'SimpleSDXL_User')
-    #sysinfo = json.loads(token.get_sysinfo().to_json())
+    token = simpleai_base.init_local(f'SimpleSDXL_User')
+    sysinfo = json.loads(token.get_sysinfo().to_json())
 # sysinfo.update(dict(did=token.get_did()))
     print(f'GPU: {sysinfo["gpu_name"]}, RAM: {sysinfo["ram_total"]}MB, SWAP: {sysinfo["ram_swap"]}MB, VRAM: {sysinfo["gpu_memory"]}MB, DiskFree: {sysinfo["disk_free"]}MB, CUDA: {sysinfo["cuda"]}')
 
