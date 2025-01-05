6d6090eb33b40b57388c946093a8e96af6ef53b1
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-05 12:29:52 -0800

    Update model_loader.py: Chinese text in console is conditional
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/model_loader.py
+++ modules/model_loader.py
@@ -68,13 +68,13 @@ def load_file_from_url(
         file_name = os.path.basename(parts.path)
     cached_file = os.path.abspath(os.path.join(model_dir, file_name))
     if not os.path.exists(cached_file):
-        print(f'Downloading: "{url}" to {cached_file}')
-        print(f'正在下载模型文件: "{url}"。如果速度慢，可终止运行，自行用工具下载后保存到: {cached_file}，然后重启应用。\n')
+        if (shared.args.language=='cn'):
+            print(f'正在下载模型文件: "{url}"。如果速度慢，可终止运行，自行用工具下载后保存到: {cached_file}，然后重启应用。\n')
+        else
+            print(f'Downloading: "{url}" to {cached_file}')
         from torch.hub import download_url_to_file
         download_url_to_file(url, cached_file, progress=progress)
         from shared import modelsinfo
         modelsinfo.refresh_file('add', cached_file, url)
 
-
     return cached_file
-
