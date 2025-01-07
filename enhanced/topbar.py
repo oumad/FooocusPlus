65f0026f814b8eeb9db8d0f1c553b922dd44480f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-06 17:32:33 -0800

    Update topbar.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -69,10 +69,9 @@ def is_models_file_absent(preset_name):
             if 'Flux' in preset_name and config_preset["default_model"]== 'auto':
                 config_preset["default_model"] = comfy_task.get_default_base_Flux_name('+' in preset_name)
             model_key = f'checkpoints/{config_preset["default_model"]}'
-            return not modelsinfo.exists_model(catalog="checkpoints", model_path=config_preset["default_model"])
-            # removed "shared" from modesinfo.exists_model
+            return not sharedmodelsinfo.exists_model(catalog="checkpoints", model_path=config_preset["default_model"])
         if config_preset["default_refiner"] and config_preset["default_refiner"] != 'None':
-           return not modelsinfo.exists_model(catalog="checkpoints", model_path=config_preset["default_refiner"])
+           return not sharedmodelsinfo.exists_model(catalog="checkpoints", model_path=config_preset["default_refiner"])
     return False
 
 
