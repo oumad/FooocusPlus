4d595ebd4fb174507c648c40ad7dbb64123189de
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-07 14:13:57 -0800

    Update meta_parser.py Simplified metadata version logging
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/meta_parser.py
+++ modules/meta_parser.py
@@ -852,7 +852,7 @@ def get_exif(metadata: str | None, metadata_scheme: str):
     exif[0x9286] = metadata
     # 0x0131 = Software
     import enhanced.version as version
-    exif[0x0131] = f'Fooocus v{fooocus_version.version} / SimpleSDXL2 v{version.get_simplesdxl_ver()} / {version.branch} v{fooocusplus_version.version}'
+    exif[0x0131] = f'Fooocus {fooocus_version.version}, SimpleSDXL2 {version.get_simplesdxl_ver()}, FooocusPlus {fooocusplus_version.version}'
     # 0x927C = MakerNote
     exif[0x927C] = metadata_scheme
     return exif
