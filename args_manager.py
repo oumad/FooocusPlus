e7b475c435c52a4f8e91a85b4cd5f93aa2d6f1b7
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-06 23:48:17 -0800

    Update args_manager.py Added the root value
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- args_manager.py
+++ args_manager.py
@@ -1,5 +1,7 @@
 import ldm_patched.modules.args_parser as args_parser
 
+root = os.path.dirname(os.path.abspath(__file__))
+
 args_parser.parser.add_argument("--preset", type=str, default='default', help="Apply specified UI preset.")
 args_parser.parser.add_argument("--disable-preset-selection", action='store_true',
                                 help="Disables preset selection in Gradio.")
