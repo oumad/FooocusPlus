e7fecaef4599b1c0cb24d9a72b79e74a19966ab0
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-09 13:57:10 -0800

    Update gallery.py Improve the English message
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/gallery.py
+++ enhanced/gallery.py
@@ -41,7 +41,7 @@ def refresh_output_list(max_per_page, max_catalog):
     output_list = sorted([f[2:] for f in listdirs1], reverse=True)
     pages = len(output_list)
     display_max_pages = max_catalog
-    print(f'[Gallery] Refresh_output_catalog: A total of {total_nums} images and {pages} pages, displaying the latest {pages if pages<display_max_pages else display_max_pages} pages.')
+    print(f'[Gallery] Refresh Output Catalog: A total of {total_nums} images and {pages} pages, displaying the latest {pages if pages<display_max_pages else display_max_pages} pages.')
     output_list = output_list[:display_max_pages]
     return output_list, total_nums, pages
 
@@ -61,7 +61,7 @@ def select_index(choice, image_tools_checkbox, state_params, evt: gr.SelectData)
     if "__output_list" in state_params.keys():
         state_params.update({"infobox_state": 0})
         state_params.update({"note_box_state": ['',0,0]})
-    print(f'[Gallery] Selected_gallery_catalog: change image catalog:{choice}.')
+    print(f'[Gallery] Selected Gallery Catalog: change image catalog:{choice}.')
     state_params.update({"gallery_state": 'finished_index'})
     return [gr.update(visible=True)] + [gr.update(visible=image_tools_checkbox)] + [gr.update(visible=False)] * 8 + [state_params]
 
@@ -74,7 +74,7 @@ def select_gallery(choice, state_params, backfill_prompt, evt: gr.SelectData):
     if choice is None and len(state_params["__output_list"]) > 0:
         choice = state_params["__output_list"][0]
     result = get_images_prompt(choice, evt.index, state_params["__max_per_page"], True)
-    #print(f'[Gallery] Selected_gallery: selected index {evt.index} of {choice} images_list:{result["Filename"]}.')
+    #print(f'[Gallery] Selected Gallery: selected index {evt.index} of {choice} images list:{result["Filename"]}.')
     if backfill_prompt and 'Prompt' in result:
         return [gr.update(value=toolbox.make_infobox_markdown(result, state_params['__theme'])), gr.update(value=result["Prompt"]), gr.update(value=result["Negative Prompt"])] + [gr.update(visible=False)] * 4 + [state_params]
     else:
@@ -107,7 +107,7 @@ def get_images_from_gallery_index(choice, max_per_page):
         else:
             images_gallery = images_list[choice][nums-max_per_page:]
     images_gallery = [os.path.join(os.path.join(config.path_outputs, "20{}".format(choice)), f) for f in images_gallery]
-    #print(f'[Gallery]Get images from index: choice={choice}, page={page}, images_gallery={images_gallery}')
+    #print(f'[Gallery]Get Images from Index: choice={choice}, page={page}, images_gallery={images_gallery}')
     return images_gallery
 
 
@@ -117,7 +117,7 @@ def refresh_images_catalog(choice: str, passthrough = False):
     if not passthrough and choice in images_list_keys:
         images_list_keys.remove(choice)
         images_list_keys.append(choice)
-        #print(f'[Gallery] Refresh_images_list: hit cache {len(images_list[choice])} image_items of {choice}.')
+        #print(f'[Gallery] Refresh Images List: hit cache {len(images_list[choice])} image items of {choice}.')
         return images_list[choice]
 
     images_list_new = sorted([f for f in util.get_files_from_folder(os.path.join(config.path_outputs, "20{}".format(choice)), image_types, None)], reverse=True)
@@ -134,7 +134,7 @@ def refresh_images_catalog(choice: str, passthrough = False):
     images_list.update({choice: images_list_new})
     images_list_keys.append(choice)
     parse_html_log(choice, passthrough)
-    print(f'[Gallery] Refresh_images_catalog: loaded {len(images_list[choice])} image_items of {choice}.')
+    print(f'[Gallery] Refresh Images Catalog: loaded {len(images_list[choice])} image_items of {choice}.')
     return images_list[choice]
 
 
@@ -166,7 +166,7 @@ def get_images_prompt(choice, selected, max_per_page, display_index=False):
     filename = images_list[choice][selected]
     metainfo = {"Filename": filename} if filename not in images_prompt[choice].keys() else images_prompt[choice][filename]
     if display_index:
-        print(f'[Gallery] The image selected: catalog={choice}, page={page_choice}, in_page={page_index}, in_catalog={selected}, filename={filename}')
+        print(f'[Gallery] The image selected: catalog={choice}, page={page_choice}, in page={page_index}, in catalog={selected}, filename={filename}')
     if choice in images_ads.keys() and filename in images_ads[choice].keys():
         metainfo.update({"Advanced_parameters": images_ads[choice][metainfo['Filename']]})
     return metainfo
@@ -179,7 +179,7 @@ def parse_html_log(choice: str, passthrough = False):
     if not passthrough and choice in images_prompt_keys and images_prompt[choice]:
         images_prompt_keys.remove(choice)
         images_prompt_keys.append(choice)
-        #print(f'[Gallery] Parse_html_log: hit cache {len(images_prompt[choice])} image_infos of {choice}.')
+        #print(f'[Gallery] Parse HTML Log: hit cache {len(images_prompt[choice])} image_infos of {choice}.')
         return
     html_file = os.path.join(os.path.join(config.path_outputs, "20{}".format(choice)), 'log.html')
     if not os.path.exists(html_file):
@@ -250,7 +250,7 @@ def parse_html_log(choice: str, passthrough = False):
                     info_dict[key] = value
             else:
                 if 'Upscale (Fast)' not in text:
-                    print(f'[Gallery] Parse_html_log: Parse error for {choice}, file={html_file}\ntext:{info.xpath(".//text()")}')
+                    print(f'[Gallery] Parse HTML Log: Parse error for {choice}, file={html_file}\ntext:{info.xpath(".//text()")}')
                 info_dict={"Filename":text[1]}
                 info_dict[text[2]] = text[3]
         #print(f'{len(text)},info_dict={info_dict}')
@@ -280,7 +280,7 @@ def parse_html_log(choice: str, passthrough = False):
             log_ext.update(json.load(log_file))
     images_ads.update({choice: log_ext})
     
-    print(f'[Gallery] Parse_html_log: loaded {len(images_prompt[choice])} image_infos of {choice}.')
+    print(f'[Gallery] Parse HTML Log: loaded {len(images_prompt[choice])} image_infos of {choice}.')
     return
 
 
