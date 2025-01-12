e3c6ae40a3cb614d80c4ffdf877e6dc68ddc53f6
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-11 16:44:53 -0800

    Update simpleai.py Use MODELSINFO global
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/simpleai.py
+++ enhanced/simpleai.py
@@ -5,6 +5,7 @@ import args_manager
 from simpleai_base import simpleai_base, utils, comfyd, torch_version, xformers_version, cuda_version, comfyclient_pipeline
 from simpleai_base.params_mapper import ComfyTaskParams
 from simpleai_base.models_info import ModelsInfo, sync_model_info
+from launch import MODELSINFO
 from build_launcher import is_win32_standalone_build
 
 args_comfyd = [[]]
@@ -13,7 +14,7 @@ modelsinfo_filename = 'models_info.json'
 def init_modelsinfo(models_root, path_map):
     global modelsinfo_filename
     models_info_path = os.path.abspath(os.path.join(models_root, modelsinfo_filename))
-    if not args_manager.modelsinfo:
-        args_manager.modelsinfo = ModelsInfo(models_info_path, path_map)
-    return args_manager.modelsinfo
+    if not MODELSINFO:
+        MODELSINFO = ModelsInfo(models_info_path, path_map)
+    return MODELSINFO
 
