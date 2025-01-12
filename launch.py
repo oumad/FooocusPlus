5dc3cb9ba20b9c81fb5886794151d6337ca70f5a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-11 16:11:53 -0800

    Update launch.py Clarified constant descriptions
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -5,10 +5,11 @@ import ldm_patched
 
 print('[System ARGV] ' + str(sys.argv))
 
-# these constants are imported to other modules
-# for example: from launch import ROOT
+# these globals are imported to other modules
+# for example: from launch import GRADIO_ROOT
 GRADIO_ROOT = None
 MODELSINFO = None
+# ROOT is a true constant, imported to other modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(ROOT)
 os.chdir(ROOT)
