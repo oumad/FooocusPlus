223536f6b8de622ab2617e96b30b227d7a2d5cbd
David Sage <djwDragonsage@telus.net>
2025-01-12 00:13:34 -0800

    Revert "Update enhanced_parameters.py Removed preselector logic"
    This reverts commit 346a5f2437c0eb4dcdb717a83bad15da1f3351ac.

--- enhanced/enhanced_parameters.py
+++ enhanced/enhanced_parameters.py
@@ -2,6 +2,11 @@ backfill_prompt, translation_methods, comfyd_active_checkbox = [None] * 3
 
 def set_all_enhanced_parameters(*args):
     global backfill_prompt, translation_methods, comfyd_active_checkbox, preselector
+    if preselector == '':
+        if lang=='cn':
+            preselector='Topbar Menu'
+        else:    
+            preselector='Dropdown Menu'
 
     backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = args
 
