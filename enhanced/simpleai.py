d2ce5fc9a1cc6560cb39ac17618b9596e5c322a0
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-11 16:48:40 -0800

    Update simpleai.py Made MODELSINFO global
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/simpleai.py
+++ enhanced/simpleai.py
@@ -13,6 +13,7 @@ modelsinfo_filename = 'models_info.json'
 
 def init_modelsinfo(models_root, path_map):
     global modelsinfo_filename
+    global MODELSINFO
     models_info_path = os.path.abspath(os.path.join(models_root, modelsinfo_filename))
     if not MODELSINFO:
         MODELSINFO = ModelsInfo(models_info_path, path_map)
