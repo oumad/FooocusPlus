e41be1cbefc4b70f7d1130fc231e54873aaefdf2
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-12 12:13:03 -0800

    Update simpleai.py Replace launch with common
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/simpleai.py
+++ enhanced/simpleai.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import gradio as gr
-import launch
+import common
 from simpleai_base import simpleai_base, utils, comfyd, torch_version, xformers_version, cuda_version, comfyclient_pipeline
 from simpleai_base.params_mapper import ComfyTaskParams
 from simpleai_base.models_info import ModelsInfo, sync_model_info
@@ -13,7 +13,7 @@ modelsinfo_filename = 'models_info.json'
 def init_modelsinfo(models_root, path_map):
     global modelsinfo_filename
     models_info_path = os.path.abspath(os.path.join(models_root, modelsinfo_filename))
-    if not launch.MODELS_INFO:
-        launch.MODELS_INFO = ModelsInfo(models_info_path, path_map)
-    return launch.MODELS_INFO
+    if not common.MODELS_INFO:
+        common.MODELS_INFO = ModelsInfo(models_info_path, path_map)
+    return common.MODELS_INFO
 
