20a62e1036842a7e82d00d48d294dd7b7bdbd24d
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-11 17:21:14 -0800

    Update enhanced_parameters.py Removed preselector function
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/enhanced_parameters.py
+++ enhanced/enhanced_parameters.py
@@ -11,12 +11,3 @@ def set_all_enhanced_parameters(*args):
     backfill_prompt, translation_methods, comfyd_active_checkbox, preselector = args
 
     return
-
-def set_preselector():
-    global preselector
-    if preselector == '':
-        if lang=='cn':
-            preselector='Topbar Menu'
-        else:    
-            preselector='Dropdown Menu'
-    return(preselector)
