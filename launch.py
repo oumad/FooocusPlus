9aaf57912628e0a9ca362ed13ac60ddb748f550e
David Sage <djwDragonsage@telus.net>
2025-01-12 00:09:35 -0800

    Revert "Update launch.py Changed MODELSINFO to MODELS_INFO for clarity"
    This reverts commit beadf0fbf59d39315c5ab4f9856aa2420df4bbcd.

--- launch.py
+++ launch.py
@@ -5,11 +5,10 @@ import ldm_patched
 
 print('[System ARGV] ' + str(sys.argv))
 
-# these globals are imported by other modules
-# and sometimes changed by them
+# these globals are imported to other modules
 # for example: from launch import GRADIO_ROOT
 GRADIO_ROOT = None
-MODELS_INFO = None
+MODELSINFO = None
 # ROOT is a true constant, imported to other modules
 ROOT = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(ROOT)
