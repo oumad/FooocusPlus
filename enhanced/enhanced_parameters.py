6af4ac7fcecf497fbd757f231f72315622c4f8b6
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-13 14:26:34 -0800

    Delete enhanced directory
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/enhanced_parameters.py
+++
@@ -1,8 +0,0 @@
-backfill_prompt, translation_methods, comfyd_active_checkbox = [None] * 3
-
-def set_all_enhanced_parameters(*args):
-    global backfill_prompt, translation_methods, comfyd_active_checkbox
-
-    backfill_prompt, translation_methods, comfyd_active_checkbox = args
-
-    return
