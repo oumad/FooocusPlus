507d32bab0aded7f3229af6e6e21b4b7a4fda90f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-13 17:03:36 -0800

    Update shared.py Restored custom path
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- custom/shared.py
+++ custom/shared.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import modules.config as config
-from launch import ROOT
+from custom import ROOT
 
 paths_checkpoints = config.paths_checkpoints
 paths_loras = config.paths_loras
