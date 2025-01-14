0c452b210301c6ffd1f1a08f787b1621b9c08cf3
David Sage <djwDragonsage@telus.net>
2025-01-13 17:28:27 -0800

    Restore Custom files

--- custom/shared.py
+++ custom/shared.py
@@ -1,14 +1,27 @@
 import os
 import sys
-import custom.shared as shared
-module_name, module_root = shared.init_module(os.path.abspath(__file__))
+from common import *
+import modules.config as config
 
-from .modules.path import PathManager
-path_manager = PathManager()
+paths_checkpoints = config.paths_checkpoints
+paths_loras = config.paths_loras
+path_embeddings = config.path_embeddings
+path_vae_approx = config.path_vae_approx
+path_upscale_models = config.path_upscale_models
+paths_inpaint = config.paths_inpaint
+paths_controlnet = config.paths_controlnet
+path_clip_vision = config.path_clip_vision
+path_fooocus_expansion = config.path_fooocus_expansion
+paths_llms = config.paths_llms
+path_outputs = config.path_outputs
 
-state = {"preview_image": None, "ctrls_name": [], "ctrls_obj": [], "pipeline": None}
+path_root = ROOT
 
-def add_ctrl(name, obj):
-    state["ctrls_name"] += [name]
-    state["ctrls_obj"] += [obj]
+def init_module(file_path):
+    module_root = os.path.dirname(file_path)
+    sys.path.append(module_root)
+    module_name = os.path.relpath(module_root, os.path.dirname(os.path.abspath(__file__)))
+    print(f'[{module_name}] The customized module:{module_name} is initializing ...')
+    return module_name, module_root
+
 
