a86d6548be1ae948a763d5b448f80d55c574f91b
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-17 12:22:59 -0800

    Update webui.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -724,7 +724,7 @@ with common.GRADIO_ROOT:
                             seed_value = int(seed_string)
                             if constants.MIN_SEED <= seed_value <= constants.MAX_SEED:
                                 return seed_value
-                        except ValueError:
+                         ValueError:
                             pass
                         return random.randint(constants.MIN_SEED, constants.MAX_SEED)
 
@@ -1027,9 +1027,9 @@ with common.GRADIO_ROOT:
 
                 refresh_files_output = [base_model, refiner_model, vae_name]
                 if not args_manager.args.disable_preset_selection:
-                    try
+                    try:
                         preset_selection
-                    except            #catch the error if a preset is not yet initialized
+                    except:           #catch the error if a preset is not yet initialized
                         preset_selection = ""
                     refresh_files_output += [preset_selection]
                 refresh_files.click(refresh_files_clicked, [state_topbar], refresh_files_output + lora_ctrls,
