9aa0da70e3ee42eda03f7f9a95e979d46a71ffba
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-18 21:10:01 -0800

    Update launch.py End of testing: Comfy lockout now occurs when VRAM<6GB
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- launch.py
+++ launch.py
@@ -151,11 +151,11 @@ def download_models(default_model, previous_default_models, checkpoint_downloads
     return default_model, checkpoint_downloads
 
 launch_vram = int(ldm_patched.modules.model_management.get_vram())
-if launch_vram<16000:
+if launch_vram<6000:
     if args.language == 'cn':
         print(f'系统GPU显存容量太小，无法正常运行Flux, SD3, Kolors和HyDiT等最新模型，将自动禁用Comfyd引擎。请知晓，尽早升级硬件。')
     else:
-        print(f'The video card appears to have',(launch_vram),'GB of memory (VRAM).')
+        print(f'The video card appears to have about',(launch_vram),'MB of memory (VRAM).')
         print('This value is too small to run Comfy based models such as Flux, SD3, Kolors, and HyDiT')
         print('so Comfy will be disabled.')
         print()
