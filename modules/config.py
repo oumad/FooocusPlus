e239c844144721269360d54568b26fc772f76406
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-23 16:58:12 -0800

    Update config.py Changed "default" preset to "Default"
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -31,10 +31,10 @@ always_save_keys = []
 visited_keys = []
 
 try:
-    with open(os.path.abspath(f'./presets/default.json'), "r", encoding="utf-8") as json_file:
+    with open(os.path.abspath(f'./presets/Default.json'), "r", encoding="utf-8") as json_file:
         config_dict.update(json.load(json_file))
 except Exception as e:
-    print(f'Load default preset failed.')
+    print(f'Loading Default preset failed.')
     print(e)
 
 try:
