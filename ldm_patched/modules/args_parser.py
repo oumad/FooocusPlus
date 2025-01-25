81c3db4327ac51e23669cbbccce97e930917cf9f
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-24 19:29:37 -0800

    Update args_parser.py
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- ldm_patched/modules/args_parser.py
+++ ldm_patched/modules/args_parser.py
@@ -41,7 +41,7 @@ parser.add_argument("--web-upload-size", type=float, default=100)
 parser.add_argument("--hf-mirror", type=str, default=None)
 
 parser.add_argument("--external-working-path", type=str, default=None, metavar="PATH", nargs='+', action='append')
-parser.add_argument("--output-path", type=str, default=os.path.join(os.pardir, 'outputs'))
+parser.add_argument("--output-path", type=str, default=os.path.join(os.pardir, 'Outputs'))
 parser.add_argument("--temp-path", type=str, default=None)
 parser.add_argument("--cache-path", type=str, default=None)
 parser.add_argument("--in-browser", action="store_true")
