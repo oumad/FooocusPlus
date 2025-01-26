c5437a21714c533ee8c12ca0794ab05e0572536a
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-26 10:15:18 -0800

    Update entry_without_update.py: Cleaned out the REMed section referring to multi-processing
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- entry_without_update.py
+++ entry_without_update.py
@@ -7,8 +7,4 @@ os.chdir(ROOT)
 
 print('Loading FooocusPlus without updating...')
 
-#if __name__ == '__main__':
-#    import multiprocessing as mp
-#    mp.set_start_method('spawn')
-
 from launch import *
