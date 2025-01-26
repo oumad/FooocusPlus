b68daabff470075ce4a366381a89444c2eda5ce6
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-26 14:19:48 -0800

    Update webui.py Improved the help text for the save_metadata_to_images checkbox
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- webui.py
+++ webui.py
@@ -295,7 +295,7 @@ with common.GRADIO_ROOT:
                         skip_button.click(skip_clicked, inputs=currentTask, outputs=currentTask, queue=False, show_progress=False)
 
                 with gr.Accordion(label='Wildcards & Batch Prompts', visible=False, open=True) as prompt_wildcards:
-                    wildcards_list = gr.Dataset(components=[prompt], type='index', label='Wildcards: [__color__:L3:4], take 3 phrases starting from the 4th in color in order. [__color__:3], take 3 randomly. [__color__], take 1 randomly.', samples=wildcards.get_wildcards_samples(), visible=True, samples_per_page=28)
+                    wildcards_list = gr.Dataset(components=[prompt], type='index', label='Wildcards', samples=wildcards.get_wildcards_samples(), visible=True, samples_per_page=28)
                     with gr.Accordion(label='Words/phrases of wildcard', visible=True, open=False) as words_in_wildcard:
                         wildcard_tag_name_selection = gr.Dataset(components=[prompt], label='Words:', samples=wildcards.get_words_of_wildcard_samples(), visible=True, samples_per_page=30, type='index')
                     wildcards_list.click(wildcards.add_wildcards_and_array_to_prompt, inputs=[wildcards_list, prompt, state_topbar], outputs=[prompt, wildcard_tag_name_selection, words_in_wildcard], show_progress=False, queue=False)
@@ -942,7 +942,7 @@ with common.GRADIO_ROOT:
                         # metadata scheme was not selectable. I fixed this by making the radio selector always visible
                         if not args_manager.args.disable_metadata:
                             save_metadata_to_images = gr.Checkbox(label='Save Metadata to Images', value=modules.config.default_save_metadata_to_images,
-                                            info='Adds parameters to generated images allowing manual regeneration.')
+                                            info='Adds parameters to an image so that it can be regenerated. A Metadata Scheme is not in effect unless this box is checked.')
                             metadata_scheme = gr.Radio(label='Metadata Scheme', choices=flags.metadata_scheme, value=modules.config.default_metadata_scheme,
                                             info='Use "simple" for compatibility with Fooocus and "a1111" for compatibility with Civitai.',
                                             visible=True)
