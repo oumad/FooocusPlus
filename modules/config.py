4e4c344611f01f91575d2acb60b8c5164161af16
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-29 19:58:07 -0800

    Update config.py Changed low VRAM preset name to "4GB_Default"
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/config.py
+++ modules/config.py
@@ -137,9 +137,9 @@ def try_get_preset_content(preset):
 available_presets = get_presets()
 preset = args_manager.args.preset
 if preset=='initial' and (int(ldm_patched.modules.model_management.get_vram())<6000)\
-and (os.path.exists("4GB_SDXL.json")):
-    preset='4GB_SDXL'
-    print('Loading the "4GB_SDXL" preset, the default for low VRAM systems')
+and (os.path.exists("4GB_Default.json")):
+    preset='4GB_Default'
+    print('Loading the "4GB_Default" preset, the default for low VRAM systems')
 config_dict.update(try_get_preset_content(preset))
 theme = args_manager.args.theme
 
