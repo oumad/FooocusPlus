501abc922e97f8a7b5b74aabe7887d4e6332be89
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-01-31 22:24:31 -0800

    Update topbar.py Removed model and model path troubleshooting code
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- enhanced/topbar.py
+++ enhanced/topbar.py
@@ -454,13 +454,6 @@ def download_models(default_model, previous_default_models, checkpoint_downloads
 
     for file_name, url in checkpoint_downloads.items():
         model_dir = os.path.dirname(common.MODELS_INFO.get_file_path_by_name('checkpoints', file_name))
-        model_temp = default_model.lower()
-        print(f'model_temp {model_temp}')
-        if (model_temp.find('flux')) and not(model_dir.endswith('\Flux')):
-            model_dir = model_dir + '\Flux'
-            os.makedirs(model_dir, exist_ok=True)            
-        print(f'model_dir {model_dir}')
-        print()
         load_file_from_url(url=url, model_dir=model_dir, file_name=os.path.basename(file_name))
     for file_name, url in embeddings_downloads.items():
         load_file_from_url(url=url, model_dir=config.path_embeddings, file_name=file_name)
