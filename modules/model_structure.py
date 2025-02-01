32d005ea9330d3bdd6a489918f204fb5c8ca93d7
David Sage <162500231+DavidDragonsage@users.noreply.github.com>
2025-02-01 12:58:44 -0800

    Update model_structure.py: Special directory creation moved here from launch.py
    def create_model_structure()
    
    Signed-off-by: David Sage <162500231+DavidDragonsage@users.noreply.github.com>

--- modules/model_structure.py
+++ modules/model_structure.py
@@ -1,3 +1,20 @@
 import os
 from modules import config
 
+def create_model_structure()
+  # ensure that all the special model directories exist
+  os.makedirs(config.paths_checkpoints[0] + '\Alternative', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\Flux', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\LowVRAM', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\Pony', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\SD1.5', exist_ok=True)
+  os.makedirs(config.paths_checkpoints[0] + '\SD3x', exist_ok=True)
+
+  # ensure that the special LoRA directories exist
+  os.makedirs(config.paths_loras[0] + '\Alternative', exist_ok=True)        
+  os.makedirs(config.paths_loras[0] + '\Flux', exist_ok=True)
+  os.makedirs(config.paths_loras[0] + '\Pony', exist_ok=True)
+  os.makedirs(config.paths_loras[0] + '\SD1.5', exist_ok=True)
+  os.makedirs(config.paths_loras[0] + '\SD3x', exist_ok=True)
+
+return
